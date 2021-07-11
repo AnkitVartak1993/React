bc9bfd1f11dee1888b6d03f7794df5eb214eb85a
Ankit <ankitvartak@gmail.com>
2021-07-11 10:35:44 -0400

    Added chakra ui

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,7 @@
 import React, {useContext, useEffect} from 'react'
 import { ShopContext }  from '../context/shopContext' 
 import { Link } from 'react-router-dom';
+import { Box, Grid, Text, Image } from '@chakra-ui/react';
 
 const HomePage = () => {
 
@@ -14,13 +15,27 @@ const HomePage = () => {
     if (!products) return <div>Loading...</div>
 
     return (
-        <div>
+        <Box>
+            <Grid templateColumns='repeat(3, 1fr)'> 
             {
                 products.map(product => (
-                    <Link to={`/products/${product.handle}` } key={product.title}>{product.title}</Link>)
+                    <Link to={`/products/${product.handle}`} key={product.id}>
+                        <Box _hover={{opacity: '80%'}} textAlign="center">
+
+                        
+                        <Image src={product.images[0].src} />
+                        <Text>
+                            {product.title}
+                        </Text>
+                        <Text>
+                            {product.variants[0].price}
+                            </Text>
+                        </Box>    
+                    </Link>)
                 )
-            }        
-        </div>
+                }
+            </Grid>    
+        </Box>
     )
 }
 
